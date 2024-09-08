bcf9b2c3b739c75ba365ea755c025618b8e991e3
Sree Satya Harsha <harsha.pss2005@gmail.com>
2024-09-08 10:04:40 +0530

    add hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,38 @@ import { IoBook } from "react-icons/io5";
 import { IoLogIn } from "react-icons/io5";
 import { IoLogOut } from "react-icons/io5";
 import { UserLoginContext } from "../../contexts/userLoginContext/userLoginContext"
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 function Header() {
   let {logoutUser,status}=useContext(UserLoginContext)
+  let [visible,setVisible]=useState(false)
+  function toggleVisible(){
+    setVisible(!visible)
+  }
   return (
     <div>
         <div className="bgc text-white d-flex justify-content-between px-5 py-3">
             <h1 className="mt-2">Find Your Flat</h1>
-            <ul className="nav fs-2">
+            {(visible===false)&&(<ul className="fs-2 head">
                 <li><Link className="nav-link text-white hov" to={'register'}><IoBook className="clr mx-2" />Register</Link></li>
                 {
                   (status===false)?
                   (<li><Link className="nav-link text-white hov" to={'login'}><IoLogIn className="clr mx-2" />Login</Link></li>):
-                  (<li><Link className="nav-link text-white hov" to={'login'}><IoLogOut className="clr mx-2" />Logout</Link></li>)
+                  (<li><Link className="nav-link text-white hov" to={'login'} onClick={logoutUser}><IoLogOut className="clr mx-2" />Logout</Link></li>)
                 }
-            </ul>
+            </ul>)}
+            <li className="fs-1 icon" onClick={toggleVisible}>☰</li>
         </div>
+        {
+          (visible===true)&&(<ul className="side fs-2">
+                <li className="fs-1 icon text-white" onClick={toggleVisible}>✕</li>
+                <li><Link className="nav-link text-white hov" to={'register'} onClick={toggleVisible}><IoBook className="clr mx-2" />Register</Link></li>
+                {
+                  (status===false)?
+                  (<li><Link className="nav-link text-white hov" to={'login'} onClick={toggleVisible}><IoLogIn className="clr mx-2" />Login</Link></li>):
+                  (<li><Link className="nav-link text-white hov" to={'login'} onClick={()=>{toggleVisible,logoutUser}}><IoLogOut className="clr mx-2" />Logout</Link></li>)
+                }
+          </ul>)
+        }
     </div>
   )
 }
