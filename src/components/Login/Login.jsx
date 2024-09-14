fcd51aa66fd48c5e2662d14d798ec1c71242c068
harsha vardhan <harshabambu@gmail.com>
2024-09-14 16:27:35 +0530

    updated forms

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,6 +3,8 @@ import { useContext, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { UserLoginContext } from '../../contexts/userLoginContext/userLoginContext'
 import { useNavigate } from 'react-router-dom'
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
 function Login() {
   let {loginUser,status}=useContext(UserLoginContext)
   let {register,handleSubmit,formState:{errors}}=useForm()
@@ -18,19 +20,21 @@ function Login() {
   return (
     <div>
       <h1 className='text-center mt-4'>Login</h1>
-      <form className='card mx-auto col-md-8 col-lg-4 col-sm-12 mt-5 bg-light p-3' onSubmit={handleSubmit(handleForm)}>
-            <div className="mb-3">
-              <label className='form-label' htmlFor="username">Username</label>
-              <input className='form-control' type="text" {...register('username',{required:true})} id="username" />
+      
+        <form id='form1' className='card mx-auto col-md-8 col-lg-4 col-sm-12 mt-5  p-3 '  onSubmit={handleSubmit(handleForm)}>
+            <div className="mb-4 mt-3">
+              <label className='form-label fs-5 fw-bold ' htmlFor="username"><FaUser className='me-2'/>Username</label>
+              <input className='form-control'  type="text" {...register('username',{required:true})} id="username" />
               {errors.username?.type==='required'&& <p className='text-danger'>*Username is required</p>}
             </div>
-            <div className="mb-3">
-              <label className='form-label' htmlFor="password">Password</label>
+            <div className="mb-4 mt-1">
+              <label className='form-label fs-5 fw-bold' htmlFor="password"><RiLockPasswordFill className='me-2'/>Password</label>
               <input className='form-control' type="password" {...register('password',{required:true})} id="password" />
               {errors.password?.type==='required'&& <p className='text-danger'>*Password is required</p>}
             </div>
-            <button type="submit" className='m-auto text-center btn btn-success'>Login</button>
+            <button id='btn1' type="submit" className='m-auto text-center btn btn-success fw-bold'>Login</button>
           </form>
+       
     </div>
   )
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,10 @@
+
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
 import { useForm } from "react-hook-form"
+import './Register.css'
+import { IoMdContact } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 function Register() {
   let {register,handleSubmit,formState:{errors}}=useForm()
   function handleForm(obj){
@@ -7,28 +13,28 @@ function Register() {
   return (
     <div>
       <h1 className="text-center mt-4">Register</h1>
-      <form className='card mx-auto col-md-8 col-lg-4 col-sm-12 mt-5 bg-light p-3' onSubmit={handleSubmit(handleForm)}>
+      <form id="form"className='card mx-auto col-md-8 col-lg-4 col-sm-12 mt-5  p-3' onSubmit={handleSubmit(handleForm)}>
         <div className="mb-3">
-          <label className='form-label' htmlFor="username">Username</label>
+          <label className='form-label fw-bold' htmlFor="username"><FaUser className='me-2'/>Username</label>
           <input className='form-control' type="text" {...register('username',{required:true})} id="username" />
           {errors.username?.type==='required'&& <p className='text-danger'>*Username is required</p>}
         </div>
         <div className="mb-3">
-          <label className='form-label' htmlFor="password">Password</label>
+          <label className='form-label fw-bold' htmlFor="password"><RiLockPasswordFill className='me-2'/>Password</label>
           <input className='form-control' type="password" {...register('password',{required:true})} id="password" />
           {errors.password?.type==='required'&& <p className='text-danger'>*Password is required</p>}
         </div>
         <div className="mb-3">
-          <label className='form-label' htmlFor="email">Email</label>
+          <label className='form-label fw-bold' htmlFor="email"><MdEmail className="me-2"/>Email</label>
           <input className='form-control' type="email" {...register('email',{required:true})} id="email" />
           {errors.email?.type==='required'&& <p className='text-danger'>*Email is required</p>}
         </div>
         <div className="mb-3">
-          <label htmlFor="contact" className="form-label">Contact number</label>
+          <label htmlFor="contact" className="form-label fw-bold"><IoMdContact className="me-2"/>Contact number</label>
           <input type="number" {...register('contact',{required:true})} id="contact" className="form-control" />
           {errors.contact?.type==='required'&& <p className='text-danger'>*Contact number is required</p>}
         </div>
-        <label htmlFor="role">Type of account</label>
+        {/* <label htmlFor="role" className="fw-bold">Type of account</label>
         <div className="mb-3 form-check">
           <div className="form-check my-3">
             <input type="radio"{...register('role',{required:true})} name="role" id="user" className="form-check-input" value={'user'}/>
@@ -39,8 +45,8 @@ function Register() {
             <label htmlFor="merchant" className="form-check-label">Merchant</label>
           </div>
           {errors.role?.type==='required'&& <p className='text-danger'>*Role is required</p>}
-        </div>
-        <button type="submit" className='m-auto text-center btn btn-success'>Register</button>
+        </div> */}
+        <button type="submit" id="btn"className='m-auto text-center btn btn-success'>Register</button>
       </form>
     </div>
   )
